35eaab00233b810338c4142c422d612abb6e63aa
sheep669 <sheep669@126.com>
2022-07-04 21:49:42 +0800

    更新首页描述

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Author: sheep669
+ * @Description: 首页模块
+ * @Date: 2022-06-30 10:17:00
+-->
 <template>
-    <div id="emo-home">emo-home page</div>
+    <div id="emo-home">emo-home page </div>
 </template>
 <script>
 export default {
