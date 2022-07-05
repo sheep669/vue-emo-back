848dfcd418fcf9da52cef84d4e48c08c78f4f887
sheep669 <sheep669@126.com>
2022-07-05 18:37:15 +0800

    生成财务模块

---
+++ src/views/finance/deal-serial.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Author: sheep669
+ * @Description: 交易流水模块
+ * @Date: 2022-7-5 18:33:28
+-->
+<template>
+    <!-- suggest to replace '_'===> '-' -->
+    <div id="deal_serial">
+        <div
+            class="right"
+            style="
+                height: 100%;
+                width: calc(100% - 160px);
+                float: right;
+                overflow-y: auto;
+            "
+        >
+            <el-card shadow="always" body-style="0px" style="margin: 8px">
+                <!-- <emo-table /> -->
+                <!-- <emo-pagination style="margin-top: 15px" /> -->
+                deal_serial page
+            </el-card>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "DealSerial",
+};
+</script>
+<style lang='less' scoped>
+</style>
