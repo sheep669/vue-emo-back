bf23d46c4e46d0a035da62a1a306c4e575569833
sheep669 <sheep669@126.com>
2022-07-05 19:14:43 +0800

    生成系统模块

---
+++ src/views/system/sys-user-operation-log.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Author: sheep669
+ * @Description: 操作日志模块
+ * @Date: 2022-7-5 19:05:02
+-->
+<template>
+    <!-- suggest to replace '_'===> '-' -->
+    <div id="sys_user_operation_log">
+        <div
+            class="right"
+            style="
+                height: 100%;
+                width: calc(100% - 160px);
+                float: right;
+                overflow-y: auto;
+            "
+        >
+            <el-card shadow="always" body-style="0px" style="margin: 8px">
+                <!-- <emo-table /> -->
+                <!-- <emo-pagination style="margin-top: 15px" /> -->
+                sys_user_operation_log page
+            </el-card>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "SysUserOperationLog",
+};
+</script>
+<style lang='less' scoped>
+</style>
---
+++ src/views/system/sys-user-view.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Author: sheep669
+ * @Description: 用户查看模块
+ * @Date: 2022-7-5 19:05:02
+-->
+<template>
+    <!-- suggest to replace '_'===> '-' -->
+    <div id="sys_user_view">
+        <div
+            class="right"
+            style="
+                height: 100%;
+                width: calc(100% - 160px);
+                float: right;
+                overflow-y: auto;
+            "
+        >
+            <el-card shadow="always" body-style="0px" style="margin: 8px">
+                <!-- <emo-table /> -->
+                <!-- <emo-pagination style="margin-top: 15px" /> -->
+                sys_user_view page
+            </el-card>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "SysUserView",
+};
+</script>
+<style lang='less' scoped>
+</style>
