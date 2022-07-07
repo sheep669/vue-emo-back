c8d285687f15a879bc02af76a3bfd8d6b81bfe60
sheep669 <sheep669@126.com>
2022-07-07 22:30:52 +0800

    完成首页设计

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+// 引入echarts
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
 import {
   Pagination,
   Dialog,
