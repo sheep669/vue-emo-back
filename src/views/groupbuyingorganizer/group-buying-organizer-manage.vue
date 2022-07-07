7c034c15ddcd40e0e14743391ed2f6fc48143758
sheep669 <sheep669@126.com>
2022-07-07 13:38:36 +0800

    完成table的封装

--- src/views/groupbuyingorganizer/group-buying-organizer-manage.vue
+++ src/views/groupbuyingorganizer/group-buying-organizer-manage.vue
@@ -6,19 +6,69 @@
 <template>
     <!-- suggest to replace '_'===> '-' -->
     <div class="group_buying_organizer_manage">
-        <el-card shadow="always" body-style="0px" style="margin: 8px">
-            <emo-table />
-            <!-- <emo-pagination style="margin-top: 15px" /> -->
-            group_buying_organizer_manage page
+        <el-card
+            shadow="always"
+            body-style="0px"
+            style="margin: 8px"
+            class="main"
+        >
+            <emo-table :config="table_config" />
+            <emo-pagination style="margin-top: 15px" />
         </el-card>
     </div>
 </template>
 <script>
 import EmoTable from "@/components/table/index";
+import EmoPagination from "@/components/pagination/index";
 export default {
     name: "GroupBuyingOrganizerManage",
     components: {
         EmoTable,
+        EmoPagination,
+    },
+    data() {
+        return {
+            table_config: {
+                thead: [
+                    {
+                        label: "会员ID",
+                        prop: "user_id",
+                        fixed: "left",
+                        width: 70,
+                    },
+                    {
+                        label: "团长姓名",
+                        prop: "group_buying_organizer_name",
+                        width: 100,
+                    },
+                    { label: "联系方式", prop: "phone_number", width: 120 },
+                    { label: "店铺名称", prop: "store_name", width: 150 },
+                    {
+                        label: "推荐团长",
+                        prop: "recommend_group_buying_organizer",
+                        width: 100,
+                    },
+                    { label: "提货地址", prop: "receiver_address", width: 230 },
+                    {
+                        label: "佣金余额",
+                        prop: "commission_balance",
+                        width: 60,
+                    },
+                    { label: "收益总额", prop: "earnings_balance", width: 60 },
+                    { label: "现金余额", prop: "cash_balance", width: 90 },
+                    {
+                        label: "团长状态",
+                        prop: "recommend_group_buying_organizer_status",
+                        type: "switch",
+                        width: 90,
+                    },
+                    { label: "备注", prop: "remark", width: 200 },
+                    { label: "申请时间", prop: "apply_time", width: 140 },
+                ],
+                checkbox: true,
+                url: `/get_table_data`,
+            },
+        };
     },
 };
 </script>
